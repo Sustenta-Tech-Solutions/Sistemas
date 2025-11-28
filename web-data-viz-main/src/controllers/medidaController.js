8cc375e0b79fc4193b252481044749eabb4df35b
LucasCieczinski <lucas.peres@sptech.school>
2025-11-28 15:32:53 -0300

    Subindo att na Dash ( integração API)

--- web-data-viz-main/src/controllers/medidaController.js
+++ web-data-viz-main/src/controllers/medidaController.js
@@ -2,13 +2,12 @@ var medidaModel = require("../models/medidaModel");
 
 function buscarUltimasMedidas(req, res) {
 
-    const limite_linhas = 7;
+    var idEmpresa = req.params.idEmpresa;
+    var qtdSilos = req.params.qtdSilos;
 
-    var idAquario = req.params.idAquario;
+    console.log(`Recuperando as ultimas medidas`);
 
-    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
-
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+    medidaModel.buscarUltimasMedidas(idEmpresa, qtdSilos).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
--- web-data-viz-main/src/models/medidaModel.js
+++ web-data-viz-main/src/models/medidaModel.js
@@ -1,15 +1,8 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idEmpresa, qtdSilos) {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+    var instrucaoSql = `SELECT * FROM vw_mediaMedicoes WHERE Empresa = ${idEmpresa} LIMIT ${qtdSilos}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
